07392c76617160c11aa5ab11af8394dd416c53e6
Nikhil Dua <nikhil.dua@observeinc.com>
2024-08-12 17:31:12 -0700

    Add dep update

--- integration/modules/deploy_helm/helm.tf
+++ integration/modules/deploy_helm/helm.tf
@@ -6,10 +6,29 @@ data "aws_eks_cluster_auth" "cluster" {
   name = "helm-charts-agent-eks"
 }
 
-# resource "helm_release" "observe-stack" {
+resource "helm_release" "observe-stack" {
+  name       = "observe-stack"
+  chart      = "${path.module}/../../../charts/stack"
+  create_namespace = true
+  namespace = "observe"
+  dependency_update = true 
+  timeout = 300 #This is default 
+  set {
+    name = "global.observe.collectionEndpoint"
+    value = "https://143958683374.collect.observe-staging.com/"
+  }
+  set {
+    name = "observe.token.value"
+    value = "ds13aZa5kaTPlP45ELKH:K3NIKVfTIJ94xh5DkFKCM9BNTgyRpXph"
+  }
+}
+
+# resource "helm_release" "observe-stack-repo" {
 #   name       = "observe-stack"
-#   chart      = "${path.module}/../../../charts/stack"
+#   repository = "https://observeinc.github.io/helm-charts"
+#   chart      = "stack"
 #   create_namespace = true
+#   dependency_update = true 
 #   namespace = "observe"
 #   timeout = 300 #This is default 
 #   set {
@@ -21,20 +40,3 @@ data "aws_eks_cluster_auth" "cluster" {
 #     value = "ds13aZa5kaTPlP45ELKH:K3NIKVfTIJ94xh5DkFKCM9BNTgyRpXph"
 #   }
 # }
-
-resource "helm_release" "observe-stack-repo" {
-  name       = "observe-stack"
-  repository = "https://observeinc.github.io/helm-charts"
-  chart      = "stack"
-  create_namespace = true
-  namespace = "observe"
-  timeout = 300 #This is default 
-  set {
-    name = "global.observe.collectionEndpoint"
-    value = "https://143958683374.collect.observe-staging.com/"
-  }
-  set {
-    name = "observe.token.value"
-    value = "ds13aZa5kaTPlP45ELKH:K3NIKVfTIJ94xh5DkFKCM9BNTgyRpXph"
-  }
-}
