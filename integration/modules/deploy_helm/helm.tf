066b7612196683d97c4a0fc6d8a03ebff98dd42f
Nikhil Dua <nikhil.dua@observeinc.com>
2024-08-16 16:49:33 -0700

    foundation for auto namesacpe desotry

--- integration/modules/deploy_helm/helm.tf
+++ integration/modules/deploy_helm/helm.tf
@@ -9,31 +9,78 @@ data "aws_eks_cluster_auth" "cluster" {
 resource "random_string" "unique_id" {
   length  = 6
   special = false
-  upper = false
+  upper   = false
 }
 
+locals {
+  helm_chart_agent_test_namespace = "helm-chart-agent-test-ns-${random_string.unique_id.result}"
+  helm_chart_agent_test_name      = "helm-chart-agent-test-${random_string.unique_id.result}"
+}
 
 resource "helm_release" "observe-agent" {
-  #name = "observe-agent"
-  name       = "helm-chart-agent-test-${random_string.unique_id.result}"
-  chart      = "${path.module}/../../../charts/agent"
-  namespace = "helm-chart-agent-test-ns-${random_string.unique_id.result}"
+  name      = local.helm_chart_agent_test_name
+  chart     = "${path.module}/../../../charts/agent"
+  namespace = local.helm_chart_agent_test_namespace
 
-  atomic = true
-  cleanup_on_fail = true
-  create_namespace = true
-  dependency_update = true 
-  timeout = 90 #k8s timeout
+  atomic            = true
+  cleanup_on_fail   = true
+  create_namespace  = true
+  dependency_update = true
+  timeout           = 90 #k8s timeout
 
   set {
-    name = "observe.collectionEndpoint"
+    name  = "observe.collectionEndpoint"
     value = var.OBSERVE_URL
   }
   set {
-    name = "observe.token"
+    name  = "observe.token"
     value = var.OBSERVE_TOKEN
   }
+  set {
+    name  = "namespaceOverride"
+    value = local.helm_chart_agent_test_namespace
+  }
+  set {
+    name  = "deployment-cluster-events.namespaceOverride"
+    value = local.helm_chart_agent_test_namespace
+  }
+  set {
+    name  = "deployment-cluster-metrics.namespaceOverride"
+    value = local.helm_chart_agent_test_namespace
+  }
+  set {
+    name  = "daemonset-logs-metrics.namespaceOverride"
+    value = local.helm_chart_agent_test_namespace
+  }
+
+  # provisioner "local-exec" {
+  #   command = "echo 'Deleting namespace ${self.metadata[0].namespace}' && kubectl delete namespace ${self.metadata[0].namespace}"
+  #   when = destroy
+  #   #on_failure = continue
+  # }
 }
+
+
+
+# resource "null_resource" "post_destroy_command" {
+#   # Force Terraform to run this resource after destroying a specific resource
+#   triggers = {
+#     always_run = "${timestamp()}" # This will ensure the null_resource runs each time
+#   }
+#   provisioner "local-exec" {
+#     command = "echo 'Deleting namespace ${helm-release.observe-agent.metadata[0].namespace}' && kubectl delete namespace ${helm-release.observe-agent.metadata[0].namespace}"
+#   }
+
+#   # Ensure this runs after your main resource is destroyed
+#   depends_on = [
+#     helm-release.observe-agent_destroyed 
+#   ]
+# }
+
+
+
+
+
 
 # resource "helm_release" "observe-stack-repo" {
 #   name       = "observe-stack"
--- integration/modules/deploy_helm/variables.tf
+++ integration/modules/deploy_helm/variables.tf
@@ -1,23 +1,23 @@
 variable "region" {
   description = "The region to deploy helm chart in"
-  type = string
-  default = "us-west-2"
+  type        = string
+  default     = "us-west-2"
 }
 
 variable "cluster_role_arn" {
   description = "The role arn to assume to access the cluster to deploy helm chart"
-  type = string
-  sensitive = true
+  type        = string
+  sensitive   = true
 }
 
 variable "OBSERVE_URL" {
   type        = string
-  sensitive = true
+  sensitive   = true
   description = "Observe URL for agent helm-chart to send data to. Eg: https://<tenant_id>.collect.observe-staging.com/"
 }
 
 variable "OBSERVE_TOKEN" {
   type        = string
-  sensitive = true
+  sensitive   = true
   description = "Observe Token for Datastream for agent helm-chart to send data to. Eg: ds1....23AB"
 } 
