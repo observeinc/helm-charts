b50a9310a5cf162072d15a8e203ec0fca7c4d615
obs-gh-nikhildua <124205220+obs-gh-nikhildua@users.noreply.github.com>
2024-08-27 09:05:15 -0700

    update helm

--- integration/modules/deploy_helm/helm.tf
+++ integration/modules/deploy_helm/helm.tf
@@ -4,36 +4,10 @@
 #   }
 # }
 
-resource "helm_release" "observe-agent" {
-  name      = var.helm_chart_agent_test_release_name
-  chart     = "${path.module}/../../../charts/agent"
-  #namespace = kubernetes_namespace.helm_namespace.metadata[0].name
-  namespace = var.helm_chart_agent_test_namespace
-
-  #atomic            = true
-  #cleanup_on_fail   = true
-  
-  create_namespace  = true #Handled by k8s resource 
-  dependency_update = true
-  timeout           = 120 #k8s timeout
-
-   values = [
-    templatefile("${path.module}/values/${var.values_file}",
-      {
-        observe_url      = var.observe_url,
-        observe_token = var.observe_token,
-        helm_chart_agent_test_namespace = var.helm_chart_agent_test_namespace
-      })
-  ]
-}
-
-
-
-
 # resource "helm_release" "observe-agent" {
 #   name      = var.helm_chart_agent_test_release_name
-#   repository = "https://observeinc.github.io/helm-charts"
-#   chart      = "agent"
+#   chart     = "${path.module}/../../../charts/agent"
+#   #namespace = kubernetes_namespace.helm_namespace.metadata[0].name
 #   namespace = var.helm_chart_agent_test_namespace
 
 #   #atomic            = true
@@ -52,6 +26,32 @@ resource "helm_release" "observe-agent" {
 #       })
 #   ]
 # }
+
+
+
+
+resource "helm_release" "observe-agent" {
+  name      = var.helm_chart_agent_test_release_name
+  repository = "https://observeinc.github.io/helm-charts"
+  chart      = "agent"
+  namespace = var.helm_chart_agent_test_namespace
+
+  #atomic            = true
+  #cleanup_on_fail   = true
+
+  create_namespace  = true #Handled by k8s resource 
+  dependency_update = true
+  timeout           = 120 #k8s timeout
+
+   values = [
+    templatefile("${path.module}/values/${var.values_file}",
+      {
+        observe_url      = var.observe_url,
+        observe_token = var.observe_token,
+        helm_chart_agent_test_namespace = var.helm_chart_agent_test_namespace
+      })
+  ]
+}
 
 
 
