6015da14802863e7c1e468ea8022eecc13ae4b41
obs-gh-nikhildua <124205220+obs-gh-nikhildua@users.noreply.github.com>
2024-09-12 12:06:49 -0700

    update pod-logs test

--- integration/scripts/test_logs.py
+++ integration/scripts/test_logs.py
@@ -9,17 +9,26 @@ import re
 def test_errors_logs(kube_client, helm_config):
      # List all pods in the specified namespace
     pods = kube_client.list_namespaced_pod(namespace=helm_config['namespace'])
+    ignore_error_patterns = [
+        "failed to retrieve ConfigMap kube-system/aws-auth"
+    ]
+
     # Iterate through each pod and retrieve the logs
     for pod in pods.items:
         pod_name = pod.metadata.name
         print(f"Pod: {pod_name}")
-        
+
         try:
             # Get logs for the pod           
-            logs = kube_client.read_namespaced_pod_log(name=pod_name, namespace=helm_config['namespace'])
-            print(logs)            
-
-            #assert "error" not in logs.lower(), f"Error found in logs of pod {pod_name}"
+            pod_logs = kube_client.read_namespaced_pod_log(name=pod_name, namespace=helm_config['namespace'])
+            # Parse the logs for any known error patterns or warning messages
+            if "error" in pod_logs.lower() or "failed" in pod_logs.lower():
+                for pattern in ignore_error_patterns:
+                    if pattern in pod_logs:
+                        print(f"Ignoring expected error '{pattern}' in logs of pod {pod_name}")
+                        continue
+                    else:
+                        pytest.fail(f"Generic error found in logs of pod {pod_name}: {pod_logs}")             
         except pytest.helpers.ApiException() as e:
             pytest.fail(f"Could not retrieve logs for pod {pod_name}: {e}")
         
