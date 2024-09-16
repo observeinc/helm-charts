fbe0d1932f1ce26e7e988f61c1d309df4340f7cf
obs-gh-nikhildua <124205220+obs-gh-nikhildua@users.noreply.github.com>
2024-09-16 15:36:55 -0700

    add expected log

--- integration/scripts/test_logs.py
+++ integration/scripts/test_logs.py
@@ -13,24 +13,33 @@ def test_errors_logs(kube_client, helm_config):
      # List all pods in the specified namespace
     pods = kube_client.list_namespaced_pod(namespace=helm_config['namespace'])
     ignore_error_patterns = [
-        "failed to retrieve ConfigMap kube-system/aws-auth"
+        "failed to retrieve ConfigMap kube-system/aws-auth" 
     ]
+    expected_good_logs = ["Starting observe-agent"] #Check observe-agent starts in every pod 
 
     # Iterate through each pod and retrieve the logs
     for pod in pods.items:
         pod_name = pod.metadata.name
-        print(f"Checking Errors in Pod: {pod_name}")   
+        print(f"\nChecking Errors in Pod: {pod_name}")   
         try:
             # Get logs for the pod           
             pod_logs = kube_client.read_namespaced_pod_log(name=pod_name, namespace=helm_config['namespace'])
+             # Check for expected logs
+            for pattern in expected_good_logs:
+                if pattern in pod_logs:
+                    print(f"Found expected log '{pattern}' in logs of pod {pod_name}")
+                else:
+                    pytest.fail(f"Expected log '{pattern}' not found in logs of pod {pod_name}: {pod_logs}")
+
             # Parse the logs for any known error patterns or warning messages
             if "error" in pod_logs.lower() or "failed" in pod_logs.lower():
                 for pattern in ignore_error_patterns:
                     if pattern in pod_logs:
                         print(f"Ignoring expected error '{pattern}' in logs of pod {pod_name}")
-                        continue
+                        #continue
                     else:
-                        pytest.fail(f"Generic error found in logs of pod {pod_name}: {pod_logs}")             
+                        pytest.fail(f"Generic error found in logs of pod {pod_name}: {pod_logs}")       
+           
         except pytest.helpers.ApiException() as e:
             pytest.fail(f"Could not retrieve logs for pod {pod_name}: {e}")
         
