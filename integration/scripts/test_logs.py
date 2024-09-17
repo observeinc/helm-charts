4c260dc4d25c275a3ee513fab9084de1fbea6974
obs-gh-nikhildua <124205220+obs-gh-nikhildua@users.noreply.github.com>
2024-09-16 17:26:18 -0700

    update script for pattern

--- integration/scripts/test_logs.py
+++ integration/scripts/test_logs.py
@@ -4,6 +4,7 @@ import pytest
 import base64 
 import json, yaml
 import re
+import time 
 
 
 @pytest.mark.tags(
@@ -14,34 +15,34 @@ def test_errors_logs(kube_client, helm_config):
     pods = kube_client.list_namespaced_pod(namespace=helm_config['namespace'])
     ignore_error_patterns = [
         "failed to retrieve ConfigMap kube-system/aws-auth",
-        "Exporting failed. Will retry the request after interval"
+        "exporting failed. Will retry the request after interval",
+        "The resourceVersion for the provided watch is too old"
     ]
-    expected_good_logs = ["Starting observe-agent"] #Check observe-agent starts in every pod 
+    expected_good_logs = [
+        "Starting observe-agent"
+    ] 
 
-    # Iterate through each pod and retrieve the logs
-    for pod in pods.items:
+    for pod in pods.items: #For each pod 
         pod_name = pod.metadata.name
-        print(f"\nChecking Errors in Pod: {pod_name}")   
+        print(f"\nChecking Logs in Pod: {pod_name}")    
         try:
-            # Get logs for the pod           
-            pod_logs = kube_client.read_namespaced_pod_log(name=pod_name, namespace=helm_config['namespace'])
-             # Check for expected logs
-            for pattern in expected_good_logs:
+            pod_logs = kube_client.read_namespaced_pod_log(name=pod_name, namespace=helm_config['namespace']) 
+            pod_logs_lines=pod_logs.splitlines()[:250]  #Get the first 250 lines 
+           
+            for pattern in expected_good_logs:    # Check that some expected logs exist 
                 if pattern in pod_logs:
                     print(f"Found expected log '{pattern}' in logs of pod {pod_name}")
                 else:
-                    pytest.fail(f"Expected log '{pattern}' not found in logs of pod {pod_name}: {pod_logs}")
+                    pytest.fail(f"Expected log '{pattern}' not found in logs of pod {pod_name}")
 
-            # Parse the logs for any known error patterns or warning messages
-            if "error" in pod_logs.lower() or "failed" in pod_logs.lower():
-                for pattern in ignore_error_patterns:
-                    if pattern in pod_logs:
-                        print(f"Ignoring expected error '{pattern}' in logs of pod {pod_name}")
-                        #continue
+            for line in pod_logs_lines:  #For each line in pod logs             
+                if "error" in line.lower() or "failed" in line.lower(): #Check for error or failed in log line 
+                    if any(pattern in line for pattern in ignore_error_patterns): 
+                        print(f"Ignoring expected error pattern log line: {line}") #Ignore expected errors 
                     else:
-                        pytest.fail(f"Generic error found in logs of pod {pod_name}: {pod_logs}")       
-           
+                        pytest.fail(f"Generic error found in logs of pod {pod_name}: {line}")             
+
         except pytest.helpers.ApiException() as e:
             pytest.fail(f"Could not retrieve logs for pod {pod_name}: {e}")
         
-    
+       
