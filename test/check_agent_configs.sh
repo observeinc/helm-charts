687fff8e30eb8b75ae5e3e77e37b91ee80d3093c
Alex Lew <alex.lew@observeinc.com>
2025-11-12 14:13:14 -1000

    fix: don't exit on error from test script

--- test/check_agent_configs.sh
+++ test/check_agent_configs.sh
@@ -32,24 +32,24 @@ any_failed=false
 confs=$(yq -e 'select(.kind == "ConfigMap").metadata.name' $1 | grep -Ev 'cluster-name|observe-agent|---')
 for conf in $confs; do
     echo "Checking $conf..."
-    yq -e "select(.kind == \"ConfigMap\" and .metadata.name == \"$conf\").data.relay" $1 > $tmp_dir/$conf.yaml
+    yq -e "select(.kind == \"ConfigMap\" and .metadata.name == \"$conf\").data.relay" "$1" > "$tmp_dir/$conf.yaml"
 
     # Remove any eks cloud detector since it errors outside of kubernetes
-    yq -i '.processors.resourcedetection/cloud.detectors=["ec2"]' $tmp_dir/$conf.yaml
+    yq -i '(.processors."resourcedetection/cloud".detectors // empty) = ["ec2"]' "$tmp_dir/$conf.yaml" 2>/dev/null || true
 
     # Remove root_path from hostmetrics since it errors outside of linux
-    yq -i 'del(.receivers.hostmetrics.root_path)' $tmp_dir/$conf.yaml
+    yq -i 'del(.receivers.hostmetrics.root_path)' "$tmp_dir/$conf.yaml" 2>/dev/null || true
 
     # Remove references to bearer_token_file for prom scraping since they may not be present
-    yq -i 'del(.receivers.prometheus/*.config.scrape_configs[].bearer_token_file)' $tmp_dir/$conf.yaml
+    yq -i 'del(.receivers.prometheus.*.config.scrape_configs[].bearer_token_file)' "$tmp_dir/$conf.yaml" 2>/dev/null || true
 
     # Remove kubeletstats since it errors outside of kubernetes :(
-    yq -i 'del(.receivers.kubeletstats*)' $tmp_dir/$conf.yaml
-    yq -i '(.service.pipelines.*.receivers[] | select(. == "kubeletstats*")) |= "nop"' $tmp_dir/$conf.yaml
+    yq -i 'del(.receivers.kubeletstats*)' "$tmp_dir/$conf.yaml" 2>/dev/null || true
+    yq -i '(.service.pipelines.*.receivers[] | select(. == "kubeletstats*")) |= "nop"' "$tmp_dir/$conf.yaml" 2>/dev/null || true
 
     # Remove loadbalancing exporter since it errors outside of kubernetes :(
-    yq -i 'del(.exporters.loadbalancing*)' $tmp_dir/$conf.yaml
-    yq -i '(.service.pipelines.*.exporters[] | select(. == "loadbalancing*")) |= "nop"' $tmp_dir/$conf.yaml
+    yq -i 'del(.exporters.loadbalancing*)' "$tmp_dir/$conf.yaml" 2>/dev/null || true
+    yq -i '(.service.pipelines.*.exporters[] | select(. == "loadbalancing*")) |= "nop"' "$tmp_dir/$conf.yaml" 2>/dev/null || true
 
     # Set various env vars to match what's provided in our helm chart pod definitions.
     if ! env MY_POD_IP=0.0.0.0 \
